5ef3f3c04ff8f9b7b9f8ee067a2c5b9015943f35
nelsonalves117 <alvesnelson@proton.me>
2024-07-31 22:57:03 -0300

    added sleep

--- nestjs-partners-api/entrypoint.sh
+++ nestjs-partners-api/entrypoint.sh
@@ -8,12 +8,18 @@ npm install &&
 
 # Run migrations
 npm run migrate:partner1 &&
+npm run migrate:partner2 &&
 
 # Start fixtures
 npm run start partner1-fixture &&
+npm run start partner2-fixture &&
 
 # Start the development servers
-npm run start:dev &&
+npm run start:dev &
+
+sleep 10
+
+npm run start:dev partner2
 
 # Wait for background processes to finish
 wait

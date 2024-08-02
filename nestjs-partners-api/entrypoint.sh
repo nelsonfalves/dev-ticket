e9b67a8a383e39d0751d119c53f5f91173700bcb
nelsonalves117 <alvesnelson@proton.me>
2024-08-02 10:57:38 -0300

    changed commands order

--- nestjs-partners-api/entrypoint.sh
+++ nestjs-partners-api/entrypoint.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install dependencies
 npm install &&
 
-# Run migrations
 npm run migrate:partner1 &&
-npm run migrate:partner2 &&
-
-# Start fixtures
 npm run start partner1-fixture &&
+
+npm run migrate:partner2 &&
 npm run start partner2-fixture &&
 
-# Start the development servers
 npm run start:dev &
 
 sleep 15
 
 npm run start:dev partner2
-
-# Wait for background processes to finish
-wait
